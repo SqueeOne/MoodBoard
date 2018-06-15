ca6bf5eb163434a74576cc2bbe7f94358a2a9d24
L3gba <loalegba@gmail.com>
2018-06-15 11:19:25 +0200

    third commit

--- src/components/MoodForm.js
+++ src/components/MoodForm.js
@@ -12,16 +12,29 @@ export default class MoodForm extends React.Component {
         const description = e.target.value;
         this.setState(() => ({ description }));
     };
+    onMoodRankChange = (e) => {
+        const moodRank = e.target.value;
+        if(moodRank.match(/^\d$/)) {
+            this.setState(() => ({ moodRank }));
+        }
+    };
     render() {
         return (
             <div className='col-6'>
                 <form>
                     <textarea 
                         paceholder="Write about what's currently on your mind."
+                        autoFocus
                         value={this.state.description}
                         onChange={this.onDescriptionChange}
                     >
                     </textarea>
+                    <input 
+                        type='text'
+                        placeholder='Rank your mood from 1 - 10'
+                        value={this.state.moodRank}
+                        onChange={this.onMoodRankChange}
+                    />
                     <button className='btn btn-lg btn-info'>Add Entry</button>
                 </form>
             </div>
